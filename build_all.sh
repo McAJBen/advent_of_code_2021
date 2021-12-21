66dc0e6bab80970c07c2ade26e4dc349658b0897
Ben McAllister <McAJBen@gmail.com>
2021-12-21 15:42:38 -0500

    Script cleanup

--- build_all.sh
+++ build_all.sh
@@ -1,49 +1,13 @@
-start=`date +%s`
+overall_start=`date +%s%N`
 
-cargo build --release --bin puzzle_1_1
-cargo build --release --bin puzzle_1_2
-cargo build --release --bin puzzle_2_1
-cargo build --release --bin puzzle_2_2
-cargo build --release --bin puzzle_3_1
-cargo build --release --bin puzzle_3_2
-cargo build --release --bin puzzle_4_1
-cargo build --release --bin puzzle_4_2
-cargo build --release --bin puzzle_5_1
-cargo build --release --bin puzzle_5_2
-cargo build --release --bin puzzle_6_1
-cargo build --release --bin puzzle_6_2
-cargo build --release --bin puzzle_7_1
-cargo build --release --bin puzzle_7_2
-cargo build --release --bin puzzle_8_1
-cargo build --release --bin puzzle_8_2
-cargo build --release --bin puzzle_9_1
-cargo build --release --bin puzzle_9_2
-cargo build --release --bin puzzle_10_1
-cargo build --release --bin puzzle_10_2
-cargo build --release --bin puzzle_11_1
-cargo build --release --bin puzzle_11_2
-cargo build --release --bin puzzle_12_1
-cargo build --release --bin puzzle_12_2
-cargo build --release --bin puzzle_13_1
-cargo build --release --bin puzzle_13_2
-cargo build --release --bin puzzle_14_1
-cargo build --release --bin puzzle_14_2
-cargo build --release --bin puzzle_15_1
-cargo build --release --bin puzzle_15_2
-cargo build --release --bin puzzle_16_1
-cargo build --release --bin puzzle_16_2
-cargo build --release --bin puzzle_17_1
-cargo build --release --bin puzzle_17_2
-cargo build --release --bin puzzle_18_1
-cargo build --release --bin puzzle_18_2
-cargo build --release --bin puzzle_19_1
-cargo build --release --bin puzzle_19_2
-cargo build --release --bin puzzle_20_1
-cargo build --release --bin puzzle_20_2
-cargo build --release --bin puzzle_21_1
-cargo build --release --bin puzzle_21_2
+for day in {1..21}
+do
+  for part in 1 2
+  do
+    cargo build --release --bin puzzle_${day}_${part}
+  done
+done
 
-end=`date +%s`
-echo "took $(expr $end - $start)s"
+echo "  overall took $(expr $(expr $(date +%s%N) - $overall_start) / 1000000) ms"
 
 read
--- run_all.sh
+++ run_all.sh
@@ -1,50 +1,15 @@
-start=`date +%s`
+overall_start=`date +%s%N`
 
-cargo run --release --bin puzzle_1_1
-cargo run --release --bin puzzle_1_2
-cargo run --release --bin puzzle_2_1
-cargo run --release --bin puzzle_2_2
-cargo run --release --bin puzzle_3_1
-cargo run --release --bin puzzle_3_2
-cargo run --release --bin puzzle_4_1
-cargo run --release --bin puzzle_4_2
-cargo run --release --bin puzzle_5_1
-cargo run --release --bin puzzle_5_2
-cargo run --release --bin puzzle_6_1
-cargo run --release --bin puzzle_6_2
-cargo run --release --bin puzzle_7_1
-cargo run --release --bin puzzle_7_2
-cargo run --release --bin puzzle_8_1
-cargo run --release --bin puzzle_8_2
-cargo run --release --bin puzzle_9_1
-cargo run --release --bin puzzle_9_2
-cargo run --release --bin puzzle_10_1
-cargo run --release --bin puzzle_10_2
-cargo run --release --bin puzzle_11_1
-cargo run --release --bin puzzle_11_2
-cargo run --release --bin puzzle_12_1
-cargo run --release --bin puzzle_12_2
-cargo run --release --bin puzzle_13_1
-cargo run --release --bin puzzle_13_2
-cargo run --release --bin puzzle_14_1
-cargo run --release --bin puzzle_14_2
-cargo run --release --bin puzzle_15_1
-cargo run --release --bin puzzle_15_2
-cargo run --release --bin puzzle_16_1
-cargo run --release --bin puzzle_16_2
-cargo run --release --bin puzzle_17_1
-cargo run --release --bin puzzle_17_2
-cargo run --release --bin puzzle_18_1
-cargo run --release --bin puzzle_18_2
-cargo run --release --bin puzzle_19_1
-cargo run --release --bin puzzle_19_2
-cargo run --release --bin puzzle_20_1
-cargo run --release --bin puzzle_20_2
-cargo run --release --bin puzzle_21_1
-cargo run --release --bin puzzle_21_2
+for day in {1..21}
+do
+  for part in 1 2
+  do
+    start=`date +%s%N`
+    cargo run --release --bin puzzle_${day}_${part}
+    echo "  took $(expr $(expr $(date +%s%N) - $start) / 1000000) ms"
+  done
+done
 
-end=`date +%s`
-echo "took $(expr $end - $start)s"
+echo "  overall took $(expr $(expr $(date +%s%N) - $overall_start) / 1000000) ms"
 
 read
-
